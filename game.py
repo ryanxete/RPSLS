d3a25d5ac0e4562fde41bf1c89660a3b2e805511
ali <aliyavuz2000@hotmail.com>
2022-07-06 10:35:37 -0400

    comments

--- game.py
+++ game.py
@@ -9,8 +9,12 @@ class Game:
 
     def run_game(self):
         self.greeting()
-        self.human.choosing()
+        ##choice method
+        if self.human.choosing() == '1':
+            print("lets start the game!")
         
+    ##choice method:
+    ##AI or Human
     def greeting(self):
         print("welcome to the game")
 
--- human.py
+++ human.py
@@ -6,3 +6,4 @@ class Human(Player):
         
     def choosing(self):
         self.chosen_gesture = input("please choose your gesture from the list for this round\n1: rock\n2: paper\n3: sciccors\n4: lizard\n5: spock\nplease enter your desired number!")
+        return self.chosen_gesture
